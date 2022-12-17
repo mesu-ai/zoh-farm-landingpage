8347673afe70ec5a49c2b323def6d0bc0a2bb8df
Md ibrahim adham mesu <mesu.s.ss@gmail.com>
2022-12-17 15:03:59 +0600

    banner add

--- src/components/molecules/Banner.jsx
+++ src/components/molecules/Banner.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import banner from '../../assets/images/banner.png'
+import banner from '../../assets/images/banner.png';
+import './Banner.scss';
+import '../../styles/responsive.scss';
 
 const Banner = () => {
   return (
-    <div style={{}}>
-      Banner
-      <img  style={{position:'absolute',top:0,right:0,width:'50vw'}} src={banner} alt="" />
-      
+    <div id='banner'>
+      <div  className='banner-container' >
+      <div className='container banner-description'
+       >
+        <h4 className='banner-title'>A New Way to Invest in Agriculture </h4>
+        <p className='banner-text'>
+          Zou provides farmes, ranchers, private foresters, and agricultural
+          producers with online self service applications and educational
+          materials.
+        </p>
+        <button className='invest-btn'>Invest Now</button>
+      </div>
+      <img
+        // style={{ position: 'absolute', top: 0, right: 0, width: '50vw' }}
+        style={{width:'100%'}}
+        src={banner}
+        alt=''
+      />
     </div>
+
+    </div>
+    
   );
 };
 
